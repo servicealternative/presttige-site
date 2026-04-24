7f6b5c9c6e5958017a924cad8f0c25dc8b1c6160
servicealternative <alternativeservice@gmail.com>
2026-04-24 16:12:18 +0400

    Header: make homepage header the single source of truth

---
+++ assets/js/site-header.js
@@ -0,0 +1,24 @@
+(function () {
+  var script = document.currentScript;
+  var variant = (script && script.getAttribute("data-variant")) || "home";
+  var isHome = variant === "home";
+
+  var headerMarkup =
+    '<header class="topbar"' + (isHome ? ' id="topbar"' : "") + ">" +
+      '<div class="topbar-inner">' +
+        '<a href="' + (isHome ? "#top" : "/") + '" aria-label="Presttige" class="topbar-logo">' +
+          '<img src="/assets/images/presttige-p-ring.png" alt="Presttige">' +
+        "</a>" +
+        (isHome
+          ? '<nav class="topbar-nav" aria-label="Primary">' +
+              '<a href="#presence" class="topbar-link">Presence</a>' +
+              '<a href="#how-it-works" class="topbar-link">How it works</a>' +
+              '<a href="#belonging" class="topbar-link">Belonging</a>' +
+            "</nav>" +
+            '<button class="topbar-cta" id="openApplyModalTop" type="button">Express interest</button>'
+          : "") +
+      "</div>" +
+    "</header>";
+
+  document.write(headerMarkup);
+})();
